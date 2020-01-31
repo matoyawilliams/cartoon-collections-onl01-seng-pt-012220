553511d67f8cfe3fe4f2679012dc53ecfa2084f4
Matoya Williams <matoyawilliams@yahoo.com>
2020-01-31 00:40:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
 # dwarves = ["Dopey", "Bashful", "Grumpy"]
-dwarves.each do |names|
-  puts names
+# 1. Dopey
+dwarves.each.with_index(1) do |names, index|
+  puts index.names
 end 
 end 
   
