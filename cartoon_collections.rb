8518f7f5ce0bab92afed8de506baa29936b7048c
Matoya Williams <matoyawilliams@yahoo.com>
2020-01-31 00:44:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
 # dwarves = ["Dopey", "Bashful", "Grumpy"]
 # 1. Dopey
 dwarves.each.with_index(1) do |names, index|
-  puts index.names
+  puts "#{index}. #{names}"
 end 
 end 
   
