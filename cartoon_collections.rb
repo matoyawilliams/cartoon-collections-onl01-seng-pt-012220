a53455bd70a74085b954207585e38c862e278d5e
Matoya Williams <matoyawilliams@yahoo.com>
2020-01-31 00:45:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,30 +8,4 @@ dwarves.each.with_index(1) do |names, index|
 end 
 end 
   
-def summon_captain_planet# code an argument here
-  # Your code here
-  planteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  summon_captain_planet(planeteer_calls)
-#=> ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-  short_words = ["puff", "go", "two"]
-long_planeteer_calls(short_words)
-
-assorted_words = ["two", "go", "industrious", "bop"]
-long_planeteer_calls(assorted_words)
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  snacks = ["crackers", "gouda", "thyme"]
-find_the_cheese(snacks)
-
-soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
-find_the_cheese(soup)
-  
-end
